5585234b682841010a22e4a172e5d4622da29ca3
Aman <amanshindehulk@gmail.com>
2024-07-07 22:30:03 +0530

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/brothers-garage/",
+  build: {
+    rollupOptions: {
+      external: ['@mui/material']
+    }
+  }
 })
